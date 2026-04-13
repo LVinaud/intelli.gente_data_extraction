e289c07b067ec3e2f41ee0112a84234ae2ebe21f
LVinaud <lazzaropereiravn@gmail.com>
2026-04-13 10:20:41 -0300

    Fixed: regic

--- InteligenteEtl/webscrapping/extractorclasses/IbgeCitiesNetworkExtractor.py
+++ InteligenteEtl/webscrapping/extractorclasses/IbgeCitiesNetworkExtractor.py
@@ -8,53 +8,75 @@ import pandas as pd
 class IbgeCitiesNetworkExtractor(AbstractDataExtractor):
 
    DATA_POINTS = {
-      "Nível da Hierarquia para as Regiões de Influência das Cidades (VAR09)" : {"column":"nivel_ori","dtype":DataTypes.STRING},
-      "Classe Denominação da Hierarquia para as Regiões de Influência das Cidades (VAR10)": {"column":"classe_ori","dtype":DataTypes.STRING}
+      "Nível da Hierarquia para as Regiões de Influência das Cidades (VAR09)" : {"column":"nivel_ori","dtype":DataTypes.INT, "sigla":"VAR09"},
+      "Classe Denominação da Hierarquia para as Regiões de Influência das Cidades (VAR10)": {"column":"classe_ori","dtype":DataTypes.INT, "sigla":"VAR10"}
    }
    EXTRACTED_CITY_CODE_COL = "cod_ori"
    DATA_CATEGORY = "Arranjos Urbanos"
 
+   # Conversão para valor numérico ordinal (1 = maior hierarquia, 11 = menor)
+   NIVEL_TO_NUM = {
+      "1A": 1, "1B": 2, "1C": 3,
+      "2A": 4, "2B": 5, "2C": 6,
+      "3A": 7, "3B": 8,
+      "4A": 9, "4B": 10,
+      "5": 11,
+   }
+   CLASSE_TO_NUM = {
+      "Grande Metrópole Nacional": 1,
+      "Metrópole Nacional":        2,
+      "Metrópole":                 3,
+      "Capital Regional A":        4,
+      "Capital Regional B":        5,
+      "Capital Regional C":        6,
+      "Centro Sub-Regional A":     7,  "Centro Subregional A": 7,
+      "Centro Sub-Regional B":     8,  "Centro Subregional B": 8,
+      "Centro de Zona A":          9,
+      "Centro de Zona B":          10,
+      "Centro Local":              11,
+   }
+
    __SCRAPER_CLASS = IbgeCitiesNetworkScrapper()
 
-   def __get_processed_collection(self,df:pd.DataFrame, data_name:str,time_series:list[int])->ProcessedDataCollection:
-      data_col_name:str = self.DATA_POINTS[data_name]["column"]
-      dtype:DataTypes =  self.DATA_POINTS[data_name]["dtype"]
+   def __get_processed_collection(self, df: pd.DataFrame, data_name: str, time_series: list[int]) -> ProcessedDataCollection:
+      data_col_name: str = self.DATA_POINTS[data_name]["column"]
+      dtype: DataTypes  = self.DATA_POINTS[data_name]["dtype"]
+      sigla: str        = self.DATA_POINTS[data_name]["sigla"]
 
-      df = df.loc[:,[self.EXTRACTED_CITY_CODE_COL,data_col_name, self.YEAR_COLUMN]] #cria novo df apenas com as colunas do código da cidade e a coluna do dado atual
-      df = df.dropna() #retira os valores nulo
+      df = df.loc[:, [self.EXTRACTED_CITY_CODE_COL, data_col_name, self.YEAR_COLUMN]]
+      df = df.dropna()
 
       df = df.rename(
          {
-            self.EXTRACTED_CITY_CODE_COL:self.CITY_CODE_COL,
-            data_col_name: self.DATA_VALUE_COLUMN 
-         }, #renomea a coluna de código do município e dos valores do dado
+            self.EXTRACTED_CITY_CODE_COL: self.CITY_CODE_COL,
+            data_col_name: self.DATA_VALUE_COLUMN
+         },
          axis="columns"
       )
-      df[self.DATA_VALUE_COLUMN] = df[self.DATA_VALUE_COLUMN].astype(dtype.value) #muda o tipo de dado da coluna de valores 
-      df[self.DATA_IDENTIFIER_COLUMN] = data_name #add coluna do nome do dado
+      conversion_map = self.NIVEL_TO_NUM if sigla == "VAR09" else self.CLASSE_TO_NUM
+      df[self.DATA_VALUE_COLUMN] = df[self.DATA_VALUE_COLUMN].map(conversion_map)
+      df[self.DATA_IDENTIFIER_COLUMN] = sigla
 
-      df = df[~df.duplicated(subset=[self.CITY_CODE_COL],keep="first")] #remove registros duplicados da coluna do codigo de municipio
+      df = df[~df.duplicated(subset=[self.CITY_CODE_COL, self.YEAR_COLUMN], keep="first")]
       df = df.reset_index(drop=True)
-      df = self.update_city_code(df, self.CITY_CODE_COL) #atualiza código do município de 6 para 7 dígitos
+      df = self.update_city_code(df, self.CITY_CODE_COL)
 
       return ProcessedDataCollection(
          category=self.DATA_CATEGORY,
          dtype=dtype,
-         data_name=data_name,
+         data_name=sigla,
          time_series_years=time_series,
          df=df
       )
 
-
    def extract_processed_collection(self) -> list[ProcessedDataCollection]:
-      data_points:list[YearDataPoint] = self.__SCRAPER_CLASS.extract_database()
-      time_series_years:list[int] = YearDataPoint.get_years_from_list(data_points)
-      df:pd.DataFrame = self._concat_data_points(data_points)
+      data_points: list[YearDataPoint] = self.__SCRAPER_CLASS.extract_database()
+      time_series_years: list[int]     = YearDataPoint.get_years_from_list(data_points)
+      df: pd.DataFrame                 = self._concat_data_points(data_points)
 
-
-      data_collections:list[ProcessedDataCollection] = []
+      data_collections: list[ProcessedDataCollection] = []
       for data_point in self.DATA_POINTS:
-          collection = self.__get_processed_collection(df,data_point,time_series_years)
-          data_collections.append(collection)
+         collection = self.__get_processed_collection(df, data_point, time_series_years)
+         data_collections.append(collection)
 
       return data_collections
--- InteligenteEtl/webscrapping/scrapperclasses/IbgeCitiesNetworkScrapper.py
+++ InteligenteEtl/webscrapping/scrapperclasses/IbgeCitiesNetworkScrapper.py
@@ -1,41 +1,60 @@
-import requests,re
+import requests, re, zipfile, io
 from .AbstractScrapper import AbstractScrapper
 import pandas as pd
 from typing import Iterable
 from datastructures import YearDataPoint
 
 
-
 class IbgeCitiesNetworkScrapper(AbstractScrapper):
 
-   URL = "https://www.ibge.gov.br/geociencias/cartas-e-mapas/redes-geograficas/15798-regioes-de-influencia-das-cidades.html?=&t=downloads"
+   URL_2018 = "https://www.ibge.gov.br/geociencias/cartas-e-mapas/redes-geograficas/15798-regioes-de-influencia-das-cidades.html?=&t=downloads"
+   URL_2007_ZIP = "https://geoftp.ibge.gov.br/organizacao_do_territorio/divisao_regional/regioes_de_influencia_das_cidades/Regioes_de_influencia_das_cidades_2007/banco_de_dados/banco_de_dados_dos_centros.zip"
 
-   def __get_file_link(self)->Iterable[tuple[str,int]]:
-      response = requests.get(self.URL) #request get pro site
-      page_html: str = response.content.decode()  #pega conteudo html
-      
-      file_regex_pattern = r"REGIC\d{4}_Ligacoes_entre_Cidades.xlsx" #regex para achar os arquivos
-      matches = list(re.finditer(file_regex_pattern,page_html))
-      
+   # Colunas padrao de saída (mesmo formato do 2018)
+   COL_COD   = "cod_ori"
+   COL_NIVEL = "nivel_ori"
+   COL_CLASSE = "classe_ori"
+
+   def __get_2018_links(self) -> Iterable[tuple[str, int]]:
+      response = requests.get(self.URL_2018)
+      page_html: str = response.content.decode()
+
+      file_regex_pattern = r"REGIC\d{4}_Ligacoes_entre_Cidades\.xlsx"
+      matches = list(re.finditer(file_regex_pattern, page_html))
+
       year_regex_pattern = r"(\d{4})"
-      get_data_year = lambda x: int(re.findall(year_regex_pattern,x)[0]) #pega o ano do dado a partir do link
-      
+      get_data_year = lambda x: int(re.findall(year_regex_pattern, x)[0])
+
       for match in matches:
          match_end = match.end()
          sliced_html = page_html[:match_end]
          url_start = sliced_html.rfind('"')
+         file_link = page_html[url_start + 1:match_end]
+         yield file_link, get_data_year(file_link)
 
-         file_link = page_html[url_start+1:match_end]
-         yield file_link , get_data_year(file_link)
-  
-   def extract_database(self)->list[YearDataPoint]:
-      data_points:list[YearDataPoint] = []
-
-      for link,year in self.__get_file_link():
+   def __extract_2018(self) -> list[YearDataPoint]:
+      data_points: list[YearDataPoint] = []
+      for link, year in self.__get_2018_links():
          df = pd.read_excel(link)
-         data_points.append(
-            YearDataPoint(df,year)
-         )
-      
+         data_points.append(YearDataPoint(df, year))
       return data_points
 
+   def __extract_2007(self) -> YearDataPoint:
+      response = requests.get(self.URL_2007_ZIP)
+      z = zipfile.ZipFile(io.BytesIO(response.content))
+      with z.open("banco_de_dados_dos_centros.xls") as f:
+         df = pd.read_excel(f, sheet_name="Banco de dados dos centros", engine="xlrd")
+
+      # Normalizar colunas para o mesmo formato do 2018
+      df = df[["codmundv", "RIC09", "RIC10"]].rename(columns={
+         "codmundv": self.COL_COD,
+         "RIC09":    self.COL_NIVEL,
+         "RIC10":    self.COL_CLASSE,
+      })
+      return YearDataPoint(df, 2007)
+
+   def extract_database(self) -> list[YearDataPoint]:
+      data_points: list[YearDataPoint] = []
+      data_points.append(self.__extract_2007())
+      data_points.extend(self.__extract_2018())
+      return data_points
